73543afaf501ea12862f2f2403d9bbaf4a43e200
Dawid Wawrzyniak <dawid.wawrzyniak@anixe.pl>
2020-06-10 07:39:48 +0200

    Updated basic ion example

--- subz_commands_examples.py
+++ subz_commands_examples.py
@@ -10,6 +10,6 @@ except ValueError:
 
 class SubzInsertBasicArizIonExample(sublime_plugin.TextCommand):
   def run(self, edit):
-    year = datetime.datetime.now().year
+    year = datetime.datetime.now().year + 1
     example = BASIC_ARIZ_ION_EXAMPLE.replace("YEAR", str(year))
     insert_ariz_example(self, edit, example)
